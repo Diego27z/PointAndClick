af5dd92f505ace75892ff1995941ef6745484ca0
Diego27z <116527185+Diego27z@users.noreply.github.com>
2024-11-07 16:08:01 -0300

    Add files via upload

--- PointAndClick/game.js
+++ PointAndClick/game.js
@@ -6,59 +6,73 @@ const scenes = [
   { background: '3.png' },
   { background: '4.png' }
 ];
-//Botones disponibles/inactivos
+//Botones y objetos disponibles/inactivos
 const mensaje = document.getElementById('mensaje');
 mensaje.style.display = 'none';
+
 const flecha = document.getElementById('flecha');
 flecha.style.display = 'none';
+
 const volver = document.getElementById('volver');
 volver.style.display = 'none';
+
 const SalirDePieza = document.getElementById('SalirDePieza');
 SalirDePieza.style.display = 'none';
+
 const Living = document.getElementById('Living');
 Living.style.display = 'none';
+
 const baño = document.getElementById('baño');
 baño.style.display = 'none';
+
 const Cuadro = document.getElementById('Cuadro');
 Cuadro.style.display = 'none';
+
 const Sudoku = document.getElementById('Sudoku');
 Sudoku.style.display = 'none';
+
 const Tele = document.getElementById('Tele');
 Tele.style.display = 'none';
+
+//Iniciar textos vacios
 let textoCompleto = "";
 let textoCompleto2 = "";
-let contador1 = 0
+//Variable nos ayuda a mostrar un texto en un momento especifico
 let haRegistrado = false;
+///
 const video = document.getElementById("miVideo");
 video.style.display = 'none';
-let detenerEscritura = false;
+//Funcion para escribir texto que se puede reutilizar
 function escribirTexto(textoCompleto) {
-  if (detenerEscritura) return;
   if (indice < textoCompleto.length) {
     mensaje.innerText += textoCompleto[indice];
     indice++;
     setTimeout(() => escribirTexto(textoCompleto, indice), 100); // Llamada recursiva con el texto y el nuevo índice
   }
 }
+//Funcion para escribir texto que solo saldrá una vez
 function escribirTexto2(textoCompleto2) {
   if (indice2 < textoCompleto2.length) {
     mensaje.innerText += textoCompleto2[indice2];
     indice2++;
     setTimeout(() => escribirTexto2(textoCompleto2, indice2), 100); // Llamada recursiva con el texto y el nuevo índice
   }
-}
-    
+}  
 const audio = new Audio('AudioRespiro.mp3');
 const playButton = document.getElementById('empezar');
 audio.loop = true;
+
+// Boton Inicial
 playButton.addEventListener('click', function() {
-  //audio.play();
+  audio.play();
   playButton.style.display = 'none';
   loadScene(currentScene);
   flecha.style.display = 'block';
   Cuadro.style.display = 'block';
   mensaje.style.display = 'Block';
 });
+
+//cargar escena al cambiar de habitacion
 function loadScene(sceneIndex) {
   mensaje.innerText = ''; // Limpia el texto anterior
   indice = textoCompleto.length;
@@ -66,20 +80,24 @@ function loadScene(sceneIndex) {
   const scene = scenes[sceneIndex];
   document.getElementById('background').src = scene.background;
 }
+
+//Click en primera flecha
   flecha.addEventListener('click', function() {
     const currentSrc = flecha.src;
     if (currentSrc.includes('flecha')) {
-      currentScene = 1; // Cambia a la escena 2
-      flecha.style.display = 'none';
+        currentScene = 1; // Cambia a la escena 2
+        //Ocultar y Mostrar elementos correspondientes
+        flecha.style.display = 'none';
         volver.style.display = 'block'; 
         SalirDePieza.style.display = 'block';
-        mensaje.innerText = ''; // Limpia el texto anterior
         Cuadro.style.display = 'none';
         Sudoku.style.display = 'block';
+        mensaje.innerText = ''; // Limpia el texto anterior
         // Cargar la nueva escena
       loadScene(currentScene); 
     }
   });
+  //Click en primera puerta
     SalirDePieza.addEventListener('click', function() {
       const currentSrc = SalirDePieza.src;
       if (currentSrc.includes('object1.png')) {
@@ -91,12 +109,13 @@ function loadScene(sceneIndex) {
           baño.style.display = 'block';
           Sudoku.style.display = 'none';
           mensaje.innerText = ''; // Limpia el texto anterior 
-          indice2 = 0; // Reinicia el índice para el efecto
+          indice2 = 0; // Reinicia el índice de donde se comenzara a escribir el texto
           loadScene(currentScene);
           textoCompleto2 = " ¡Alo!, ¿hay alguien por ahi?";
           escribirTexto2(textoCompleto2);
       }
     });
+    //Click para ir al Living
         Living.addEventListener('click', function() {
           const currentSrc = Living.src;
           if (currentSrc.includes('object1.png')) {
@@ -110,6 +129,7 @@ function loadScene(sceneIndex) {
               video.style.display = 'block';
               video.play();
               video.playbackRate = 1.4;
+              //Funcion que muestra un texto cuando el video llega a los 5 segundos
               video.addEventListener("timeupdate", () => {
                 if (video.currentTime >= 5 && !haRegistrado) {
                   mensaje.innerText = ''; // Limpia el texto anterior
@@ -121,23 +141,27 @@ function loadScene(sceneIndex) {
             loadScene(currentScene); // Cargar la nueva escena
           }
         });
+        //Click en Cuadro de esposa
         Cuadro.addEventListener('click', function() {
           mensaje.innerText = ''; // Limpia el texto anterior
           indice = 0; // Reinicia el índice para el efecto
           textoCompleto = "Es... es ella, ¿no? Mi... mi querida...";
           escribirTexto(textoCompleto); // Inicia el efecto de escritura
         });
+        //Click en Sudoku
         Sudoku.addEventListener('click', function() {
           mensaje.innerText = ''; // Limpia el texto anterior
           indice = 0; // Reinicia el índice para el efecto
           textoCompleto = "Me dijeron que esto me ayudaría, pero ¿para qué?";
           escribirTexto(textoCompleto); // Inicia el efecto de escritura
         });
+        //Click en "volver" que va variando segun en la habitacion donde uno se encuentre
 volver.addEventListener('click', function() {
   const currentSrc = volver.src;
   if (currentSrc.includes('object2.png')) {
     if(currentScene==1){
-      currentScene = 0;
+      currentScene = 0;  //representa a la habiatacion a la cual queremos ir
+      //Ocultar y Mostrar elementos correspondientes
       volver.style.display = 'none';
       flecha.style.display = 'block';
       SalirDePieza.style.display = 'none';
